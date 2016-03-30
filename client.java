d63ee0caaec12bdbcc5b69afaf252e3386353eba
Mário Ferreira <m.j.v.ferreira@hotmail.com>
2016-03-30 09:56:43 +0100

    Modified client

--- client.java
+++ client.java
@@ -8,25 +8,63 @@ public class client {
 
     private client() {}
 
+    public  static void menu(String host, String command){
+        String response;
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Hello stub = (Hello) registry.lookup("Hello");
+            switch(command) {
+                case "HELLO":
+                    response = stub.sayHello();
+                    System.out.println("response: " + response);    
+                    break; 
+                case "REGISTER":
+                    response = stub.register();
+                    System.out.println("response: " + response); 
+                    break;
+                case "CONSULT REQUEST":
+                    response = stub.consultRequest();
+                    System.out.println("response: " + response); 
+                    break;
+                case "CONSULT RESPONSE":
+                    response = stub.consultResponse();
+                    System.out.println("response: " + response); 
+                    break;
+                case "PROBE REQUEST":
+                    response = stub.probeRequest();
+                    System.out.println("response: " + response); 
+                    break;
+                case "PROBE RESPONSE":
+                    response = stub.probeResponse();
+                    System.out.println("response: " + response); 
+                    break;
+                case "REQUEST":
+                    response = stub.request();
+                    System.out.println("response: " + response); 
+                    break;
+                case "DATA":
+                    response = stub.data();
+                    System.out.println("response: " + response); 
+                    break;
+            }
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+
+    }
     //Cliente
     public static void main(String[] args) {
-
         String host = (args.length < 1) ? null : args[0];
         String command, operation;
         try {
-            Registry registry = LocateRegistry.getRegistry(host);
+           
             Scanner scanner = new Scanner(System.in);
-            String response;
-            Hello stub = (Hello) registry.lookup("Hello");
 
             while(!(command = scanner.nextLine()).toUpperCase().equals("END")){
                 operation = command.toUpperCase();
-                switch(operation) {
-                 case "HELLO":
-                     response = stub.sayHello();
-                     System.out.println("response: " + response);    
-                     break; 
-                                 } 
+                if (operation.equals("EXIT")) break;
+                else menu(host,operation);
             }
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
